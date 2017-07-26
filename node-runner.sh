fc6cab2a5ef18c2a259f2d558e45b2d491626d1d
Rob Vicchiullo <robv@Robs-MacBook-Pro.local>
2017-07-26 15:35:50 -0700

    init

---
+++ node-runner.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+NODE_RUNNER_HOME=/etc/ansible 
+
+function bailout() {
+	echo "node-runner: $*" >&2
+	exit 2
+}
+
+git=/usr/bin/git
+
+
+if [ -d "$NODE_RUNNER_HOME" ]; then
+	source $NODE_RUNNER_HOME/node-runner.cf
+else
+	echo "$0: \$NODE_RUNNER_HOME is not a directory." >&2
+	exit 1
+fi
+
+[ -x $git ] || bailout "Can't find $git or is not executable"
+
+cd $NODE_RUNNER_HOME
+$git pull --quiet
+
+# Re-read our config, as it may have changed after pull
+
+source $NODE_RUNNER_HOME/node-runner.cf
+
+# todo: maybe check md5sum of node-runner.sh before and after pull to
+# decide if we want to re-exec node-runner.sh :) 
+
+
+# Run the playbook
+
+ansible-playbook ${playbook}
