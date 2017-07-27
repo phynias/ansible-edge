baf0cb523eebdcd2e6ab2ec76d51b7b81b58c2b5
phynias <phynias@gmail.com>
2017-07-26 17:29:03 -0700

    Update node-runner.sh
    added sleep

--- node-runner.sh
+++ node-runner.sh
@@ -8,4 +8,4 @@ git=/usr/bin/git
 [ -x $git ] || bailout "Can't find $git or is not executable"
 
 # Run the playbook
-ansible-pull -o -C master -d $NODE_RUNNER_HOME -i $NODE_RUNNER_HOME/hosts -U https://github.com/phynias/ansible-edge.git >> /var/log/ansible-pull.log 2>&1
+ansible-pull -s 5 -o -C master -d $NODE_RUNNER_HOME -i $NODE_RUNNER_HOME/hosts -U https://github.com/phynias/ansible-edge.git >> /var/log/ansible-pull.log 2>&1
